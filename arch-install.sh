6d47dbe9009d08dfd1b7974a21b8c948f51ee80a
Prem Kumar <prmsrswt@gmail.com>
2018-08-18 05:24:18 +0530

    using gdisk instead of fdisk, updated pacman database after enabling multilib

--- arch-install.sh
+++ arch-install.sh
@@ -9,7 +9,7 @@ function partion {
 	case "$resp" in
 	    [yY][eE][sS]|[yY])
 		read -r -p "which drive you want to partition? " drive
-	        fdisk $drive
+	        gdisk $drive
 	        ;;
 	    *)
 	        ;;
@@ -41,11 +41,11 @@ function base {
 function archroot {
 	read -r -p "Enter the username: " uname
 	read -r -p "Enter the hostname: " hname
-	arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime && hwclock --systohc && nano /etc/locale.gen && locale-gen && echo 'LANG=en_US.UTF-8' > /etc/locale.conf && echo $hname > /etc/hostname && echo 127.0.0.1	$hname >> /etc/hosts && echo ::1	$hname >> /etc/hosts && echo 127.0.1.1	$hname.localdomain	$hname >> /etc/hosts && passwd && useradd --create-home $uname && passwd $uname && groupadd sudo && gpasswd -a $uname sudo && EDITOR=vim visudo && pacman -S gnome gnome-tweaks papirus-icon-theme ttf-hack && systemctl enable gdm NetworkManager bluetooth && vim /etc/pacman.conf && sleep 1 && vim /etc/gdm/custom.conf && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch && grub-mkconfig -o /boot/grub/grub.cfg && exit"
+	arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime && hwclock --systohc && nano /etc/locale.gen && locale-gen && echo 'LANG=en_US.UTF-8' > /etc/locale.conf && echo $hname > /etc/hostname && echo 127.0.0.1	$hname >> /etc/hosts && echo ::1	$hname >> /etc/hosts && echo 127.0.1.1	$hname.localdomain	$hname >> /etc/hosts && passwd && useradd --create-home $uname && passwd $uname && groupadd sudo && gpasswd -a $uname sudo && EDITOR=vim visudo && pacman -S gnome gnome-tweaks papirus-icon-theme ttf-hack && systemctl enable gdm NetworkManager bluetooth && vim /etc/pacman.conf && pacman -Syu && sleep 1 && vim /etc/gdm/custom.conf && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch && grub-mkconfig -o /boot/grub/grub.cfg && exit"
 }
 
 function browser {
-	read -r -p "Install firefox? " ff
+	read -r -p "Install firefox? [y/N] " ff
 	case "$ff" in
 	    [yY][eE][sS]|[yY])
 	        arch-chroot /mnt bash -c "pacman -S firefox && exit"
@@ -53,7 +53,7 @@ function browser {
 	    *)
 	        ;;
 	esac
-	read -r -p "Instsll chromium? " chrom
+	read -r -p "Install chromium? [y/N] " chrom
 	case "$chrom" in
 	    [yY][eE][sS]|[yY]) 
 	        arch-chroot /mnt bash -c "pacman -S chromium && exit"
@@ -67,7 +67,7 @@ function graphics {
 	read -r -p "Do you want proprietary nvidia drivers? " graphic
 	case "$graphic" in
 	    [yY][eE][sS]|[yY]) 
-	        arch-chroot /mnt bash -c "pacman -S nvidia nvidia-settings nvidia-utils lib32-nvidia-utils && exit"
+	        arch-chroot /mnt bash -c "pacman -Sy nvidia nvidia-settings nvidia-utils lib32-nvidia-utils && exit"
 	        ;;
 	    *)
 	        ;;
@@ -78,7 +78,7 @@ function installsteam {
 	read -r -p "Do you want to install steam? " isteam
 	case "$isteam" in
 	    [yY][eE][sS]|[yY])
-	        arch-chroot /mnt bash -c "pacman -S steam lib32-gtk2 lib32-gtk3 lib32-libpulse lib32-libvdpau lib32-libva lib32-libva-vdpau-driver lib32-openal && exit"
+	        arch-chroot /mnt bash -c "pacman -Sy steam lib32-gtk2 lib32-gtk3 lib32-libpulse lib32-libvdpau lib32-libva lib32-libva-vdpau-driver lib32-openal && exit"
 	        ;;
 	    *)
 	        ;;
